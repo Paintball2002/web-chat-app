4cad2d53ffd67e31a38c2b807d16ee70eba56965
CCole <connor.coleman2002@gmail.com>
2018-07-14 23:04:46 +0100

    New Location Feature

--- public/js/index.js
+++ public/js/index.js
@@ -16,6 +16,17 @@ socket.on('newMessage', function (message) {
   jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function (message) {
+  console.log('newLocationMessage', message);
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My Current Location</a>');
+  li.text(`${message.from}: `);
+  a.attr("href", message.url);
+  li.append(a);
+
+  jQuery('#messages').append(li);
+});
+
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
@@ -25,4 +36,20 @@ jQuery('#message-form').on('submit', function (e) {
   }, function () {
 
   });
+});
+
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function(e) {
+  if (!navigator.geolocation){
+    return alert("Error: Your browser does not support this!");
+  }
+  navigator.geolocation.getCurrentPosition(function (pos) {
+    socket.emit("createLocationMessage", {
+      lat : pos.coords.latitude,
+      long: pos.coords.longitude
+    });
+  }, function (){
+    alert("Error: Unable to get location!")
+  })
 });
