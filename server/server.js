38de0c33f17b685051360c66b7d48ba5633638aa
CCole <connor.coleman2002@gmail.com>
2018-07-14 14:41:31 +0100

    Add connect & disconnect

--- server/server.js
+++ server/server.js
@@ -9,11 +9,18 @@ const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
-
-
+var io = socketIO(server);
 
 
 app.use(express.static(publicPath));
+
+io.on("connection", (socket) => {
+  console.log("New User Connected");
+
+  socket.on("disconnect", () => {
+    console.log("User Connection Disconnected!")
+  });
+});
 
 server.listen(port, () => {
   console.log(`Server is up on port ${port} `)
