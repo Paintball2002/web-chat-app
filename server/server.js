7efa1cdd1eb60b955f71c1e8093239871a53b60d
CCole <connor.coleman2002@gmail.com>
2018-07-14 20:20:48 +0100

    Add New Events

--- server/server.js
+++ server/server.js
@@ -15,6 +15,18 @@ app.use(express.static(publicPath));
 io.on("connection", (socket) => {
   console.log("New User Connected");
 
+  socket.emit("newMessage", {
+    from: "Admin",
+    text: "Welcome to the chat app",
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit("newMessage", {
+    from: "Admin",
+    text: "New user Joined",
+    createdAt: new Date().getTime()
+  })
+
 
   socket.on("createMessage", (message) => {
     console.log("CreateMessage", message)
@@ -23,6 +35,11 @@ io.on("connection", (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit("newMessage", {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on("disconnect", () => {
