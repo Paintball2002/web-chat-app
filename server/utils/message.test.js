ec32b867e8bddee0ece8ffc6ef230ec6fe583998
CCole <connor.coleman2002@gmail.com>
2018-07-15 14:08:26 +0100

    update Location Feature

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -9,5 +9,17 @@ describe('generateMessage', () => {
     var message = generateMessage(from, text);
     expect(typeof message.createdAt).toBe("number");
     expect(message).toMatchObject({from, text});
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'Jonny';
+    var lat = 150;
+    var long = 1000;
+    var url = "https://www.google.com/maps?q=150,1000";
+    var message = generateLocationMessage(from, lat, long);
+    expect(typeof message.createdAt).toBe("number");
+    expect(message).toMatchObject({from, url});
   });
 });
